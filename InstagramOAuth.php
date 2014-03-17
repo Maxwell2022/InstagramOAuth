ead2f44c848e0f0fcb2c6e40140c4b21ef52e15b
Maxime Aoustin <max44410@gmail.com>
2014-03-17 17:10:59 +1100

    Add Exception and fix requests

--- InstagramOAuth.php
+++ InstagramOAuth.php
@@ -3,7 +3,7 @@
 namespace Maxwell\Instagram;
 
 use Maxwell\OAuthClient\AbstractOAuthClient;
-use Maxwell\OAuthClient\OAuth;
+use Maxwell\Instagram\Exception\InstagramException;
 
 /**
  * Instagram OAuth class
@@ -14,20 +14,19 @@ class InstagramOAuth extends AbstractOAuthClient
     const AUTHENTICATE_URL = 'https://api.instagram.com/oauth/authorize';
     const AUTHORIZE_URL = 'https://api.instagram.com/oauth/authorize';
     const REQUEST_TOKEN_URL = 'https://api.instagram.com/oauth/authorize';
-
     const API_ENTRY_POINT = 'https://api.instagram.com/v1/';
 
     /**
      * Get the authorize URL
      *
-     * @param null $redirectURI
+     * @param null $redirectURL
      * @param string $scope
      * @param null $state
      * @param string $responseType
      *
      * @return string
      */
-    function getAuthorizeURL($redirectURI = null, $scope = 'basic', $state = null, $responseType = 'code')
+    function getAuthorizeURL($redirectURI=null, $scope='basic', $state=null, $responseType='code')
     {
         $params = array(
             'client_id' => $this->getConsumerKey(),
@@ -44,15 +43,13 @@ class InstagramOAuth extends AbstractOAuthClient
     }
 
     /**
-     * Exchange request token and secret for an access token and
-     * secret, to sign API calls.
+     * Exchange request code for an access token
      *
      * @param string $code
-     * @param string|null $redirectURI
-     *
+     * @param null $redirectURI
      * @return mixed
      */
-    function getAccessToken($code, $redirectURI = null)
+    function getAccessToken($code, $redirectURI=null)
     {
         $params = array(
             'code' => $code,
@@ -66,19 +63,17 @@ class InstagramOAuth extends AbstractOAuthClient
     }
 
     /**
-     * Request the API
+     * Format API request
      *
-     * @param $uri
-     * @param string $method
-     * @param array $parameters
-     * @return array|string
+     * @param $url
+     * @param $method
+     * @param $parameters
+     * @return API
      */
-    public function request($uri, $method='GET', $parameters=array())
+    public function request($uri, $method='GET', $parameters=array(), $headers=array(), $rawencoding=null)
     {
         // Query parameters needed to make a basic OAuth transaction
-        $params = array(
-            'format' => self::DEFAULT_FORMAT,
-        );
+        $params = array();
 
         $parameters = array_merge($params, $parameters);
         $url = $uri;
@@ -97,9 +92,9 @@ class InstagramOAuth extends AbstractOAuthClient
         // build the http query
         if ('GET' === strtoupper(trim($method))) {
             $url .= '?' . http_build_query($parameters);
-            $response = $this->http($url, 'GET');
+            $response = $this->http($url, 'GET', null, $headers);
         } else {
-            $response = $this->http($url, $method, $parameters);
+            $response = $this->http($url, $method, $parameters, $headers);
         }
 
         if ('json' == self::DEFAULT_FORMAT) {
@@ -115,10 +110,10 @@ class InstagramOAuth extends AbstractOAuthClient
      *
      * @param $data
      * @return bool|void
-     * @throws \Exception
+     * @throws InstagramException
      */
     public function publish($data)
     {
-        throw new \Exception('Instagram does not support publication through API');
+        throw new InstagramException('Instagram does not support publication through API');
     }
 }
